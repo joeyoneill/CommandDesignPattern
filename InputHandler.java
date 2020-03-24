295c0a70a2becea193be513ea6ce207af6fabf91
Charles Coffin <cdcoffin@email.sc.edu>
2020-03-24 01:52:10 -0400

    added InputHandler and hashmapping

---
+++ InputHandler.java
@@ -0,0 +1,45 @@
+package csce247.assignments.command;
+import java.util.HashMap; 
+
+/**
+ * @author Charlie Coffin
+ * @version 1.0
+ * @since 1.0
+ */
+public class InputHandler {
+
+	public HashMap<String, Command> commands;
+	
+	public InputHandler(Document document) {
+		commands = new HashMap<String, Command>();
+		
+		Command load = new LoadCommand(document);
+		Command save = new SaveCommand(document);
+		Command print = new PrintCommand(document);
+		Command spellCheck = new SpellCommand(document);
+		
+		commands.put("load", load);
+		commands.put("save", save);
+		commands.put("print", print);
+		commands.put("spell", spellCheck);
+	} 
+	
+	public void inputEntered(String data) {
+		
+		if(data.equalsIgnoreCase("quit")){
+			System.out.println("Goodbye");
+			System.exit(0);
+		}
+		
+		Command command = commands.get(data); 
+		
+		if(command == null) {
+			System.out.println("Sorry, we don't recognize that command");
+			return;
+		}
+		
+		else {
+			command.execute();
+		}
+	}
+}
